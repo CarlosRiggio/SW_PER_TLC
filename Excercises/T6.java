8a5ecd374b5848dcc105fbf918eee85271ab7b8d
CarlosRiggio <criggio02@gmail.com>
2023-10-02 13:16:12 +0200

    t6 done

--- Excercises/T6.java
+++ Excercises/T6.java
@@ -5,7 +5,8 @@ public class T6 {
     {
         // DisplayStars4();
         // ej3();
-        ej5();
+        // ej5();
+        ej8();
     }
 
     public static void ej3()
@@ -46,8 +47,10 @@ public class T6 {
                     }
 
                 System.out.print("Do you want to execute the programm again?[Y/N]:");
-                char answer = scan.next().charAt(0);
-                if(answer == 'N')
+                scan.nextLine();
+                String answer = scan.nextLine();
+                answer = answer.toUpperCase();
+                if(answer.equals("N"))
                 {
                     break;
                 }
@@ -59,6 +62,50 @@ public class T6 {
             // scan.close();    
         }
         
+    }
+
+    public static void ej8()
+    {
+        Scanner scan = new Scanner(System.in);
+        int answer = 0;
+        int radius = 0;
+        double pi = 3.14;
+        while(answer != 5) {
+            System.out.println("Select an option:");
+            System.out.println("[1] Set radius");
+            System.out.println("[2] Display radius");
+            System.out.println("[3] Display area");
+            System.out.println("[4] Display perimeter");
+            System.out.println("[5] Quit");
+
+            answer = scan.nextInt();
+
+            switch(answer)
+            {
+                case 1: 
+                    System.out.print("Introduce the value of the radio:");
+                    radius = scan.nextInt();
+                    break;
+                case 2: 
+                    System.out.println("The radius value is: "+radius );
+                    break;
+                case 3:
+                    double area = pi*(radius*radius);
+                    System.out.println("The area of the circunference is: "+ area);
+                    break;
+                case 4:
+                    double perimeter = 2*pi*radius;
+                    System.out.println("The perimeter of the circunference is: "+ perimeter);
+                    break;
+                case 5: 
+                    break;
+
+            }
+
+
+
+            
+        } 
     }
     
 
